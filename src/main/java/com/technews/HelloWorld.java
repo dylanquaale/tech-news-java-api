823abb8a1c2fca3ecd821ddffe20fc6579e3511f
dylanquaale <dylanquaale33@gmail.com>
2023-06-06 12:45:23 -0500

    Created new class

--- src/main/java/com/technews/HelloWorld.java
+++ src/main/java/com/technews/HelloWorld.java
@@ -20,11 +20,14 @@ public class VariableTypes {
 
 public class VariableTypes {
     public static void main(String[] args) {
-    String name = "Cleopatra";
-        int age = 2016 + 69;
-        double price = 1999.99;
-        boolean truth = true;
-        boolean falseness = !truth;
+      int a = 4;
+      double b = 3.5;
+      String c = "cat";
 
+      double sum = a + b;
+      String concat = a + c;
+
+      System.out.println(sum);
+      System.out.println(concat);
     }
 }
